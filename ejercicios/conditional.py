ed39a15f7110149af2b122f09410a9b7dab6ab1a
Jose <jose.manuel@holafly.com>
2023-03-18 12:06:46 +0100

    conditionals

---
+++ ejercicios/conditional.py
@@ -0,0 +1,13 @@
+'''
+Dada una edad, se le dice al portero dicha edad
+
+Si es mayor de edad podrá entrar en la discoteca
+Si no es mayor de edad no podrá entrar en la discoteca.
+'''
+age = 17
+
+if age >= 18:
+    print('Puedes entrar')
+else:
+    print('no puedes entrar')
+
