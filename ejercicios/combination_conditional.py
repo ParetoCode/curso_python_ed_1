a3e701cbd2d689cf63a69d8935b6397a1b1d7dbe
Jose <jose.manuel@holafly.com>
2023-03-18 12:35:29 +0100

    Functions explained

---
+++ ejercicios/combination_conditional.py
@@ -0,0 +1,18 @@
+'''
+2 variables:
+edad:
+examen de conducción:
+Para que alguien conduzca tiene que:
+- Ser mayor de edad
+- Haber pasado el examen de conducción
+'''
+ADULT_AGE = 18
+
+age = 19
+exam_is_passed = False
+
+
+if age >= ADULT_AGE and exam_is_passed:
+    print('Puedes conducir')
+else:
+    print('No puedes conducir')
